595a665ed14357d1109e585d8c05ae59c8abcb73
Nima Mehanian <nima.mehanian@gmail.com>
2019-06-21 21:54:01 -0700

    Make TikTok text style effect for placeholder

--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -1,4 +1,9 @@
-export const test = '';
+export const tikTokTextEffect = `
+  font-size: 64px;
+  text-shadow: -4px -2px 0px cyan, 4px 2px 0px red;
+  color: #fff;
+  background: #232323;
+`;
 
 export const disableHighlight = `
   -webkit-user-select: none;
